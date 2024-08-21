9157c66141bc3fa7c611e1b91195d0e8f873d233
maorbeeri <marmaor@gmail.com>
2024-08-22 01:43:58 +0300

    finshed

--- src/pages/PresentationPreview.tsx
+++ src/pages/PresentationPreview.tsx
@@ -4,7 +4,11 @@ import { Dropdown } from "primereact/dropdown";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
 
+import { Dialog } from "primereact/dialog";
+
 import axios from "axios";
+import Header from "../components/Header";
+import { InputTextarea } from "primereact/inputtextarea";
 
 interface Slide {
   _id: string; // Unique ID for each slide
@@ -26,6 +30,10 @@ function PresentationPreview() {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
   const [newSlideContent, setNewSlideContent] = useState("");
   const [editSlideContent, setEditSlideContent] = useState("");
+
+  const [isAddDialogVisible, setAddDialogVisible] = useState(false);
+  const [isEditDialogVisible, setEditDialogVisible] = useState(false);
+
   const [slideToEdit, setSlideToEdit] = useState<number | null>(null);
   const navigate = useNavigate();
 
@@ -76,6 +84,7 @@ function PresentationPreview() {
             });
             setNewSlideContent("");
             setCurrentSlideIndex(response.data.presentation.slides.length - 1);
+            setAddDialogVisible(false);
           });
       } catch (error) {
         console.error("Error adding slide:", error);
@@ -88,13 +97,17 @@ function PresentationPreview() {
     if (presentation && slideToEdit !== null) {
       try {
         const slideId = presentation.slides[slideToEdit]._id;
-        const response = await axios.patch<Presentation>(
+        const response = await axios.patch(
           `http://localhost:3000/presentations/${presentation.title}/slides/${slideId}`,
           { content: editSlideContent }
         );
-        setPresentation(response.data);
+        setPresentation({
+          ...presentation,
+          slides: response.data.presentation.slides,
+        });
         setEditSlideContent("");
         setSlideToEdit(null);
+        setEditDialogVisible(false);
       } catch (error) {
         console.error("Error altering slide:", error);
       }
@@ -132,97 +145,157 @@ function PresentationPreview() {
 
   if (!presentation) return <div>Loading...</div>;
 
+  const header = (
+    <>
+      <Button
+        rounded
+        onClick={handlePreviousSlide}
+        disabled={currentSlideIndex === 0}
+        severity="secondary"
+      >
+        Previous
+      </Button>
+      <Button
+        rounded
+        severity="secondary"
+        onClick={handleNextSlide}
+        disabled={currentSlideIndex === presentation.slides.length - 1}
+        style={{ marginLeft: "0.5em" }}
+      >
+        Next
+      </Button>
+    </>
+  );
+
+  const footer = (
+    <>
+      <Button
+        label="Add Slide"
+        severity="success"
+        rounded
+        onClick={() => setAddDialogVisible(true)}
+      />
+      <Button
+        label="Edit"
+        severity="info"
+        rounded
+        style={{ marginLeft: "0.5em" }}
+        onClick={() => {
+          setSlideToEdit(currentSlideIndex);
+          setEditDialogVisible(true);
+        }}
+      />
+      <Button
+        label="Delete"
+        severity="danger"
+        rounded
+        style={{ marginLeft: "0.5em" }}
+        onClick={() => handleDeleteSlide(currentSlideIndex)}
+      />
+    </>
+  );
+
+  const closeDialog = (type: string) => {
+    if (type === "edit") {
+      setEditDialogVisible(false);
+    } else {
+      setAddDialogVisible(false);
+    }
+  };
+
+  const noSlides = (
+    <>
+      <Card style={{ marginTop: "10px" }}>
+        <p>noSlides!</p>
+        <Button
+          label="Add Slide"
+          severity="success"
+          rounded
+          onClick={() => setAddDialogVisible(true)}
+        />
+      </Card>
+    </>
+  );
+
   return (
     <div>
+      <button onClick={handleBackToDashboard}>Back to Dashboard</button>
+
       <Card title={presentation.title}>
         <p>Authors: {presentation?.authors?.join(", ") || "No authors"}</p>
         <p>
           Published on: {new Date(presentation.createdAt).toLocaleDateString()}
         </p>
       </Card>
-
-
-
       <div>
         {presentation.slides.length ? (
-          <div>
-            <h2>Slide {currentSlideIndex + 1}</h2>
-            <p>{presentation.slides[currentSlideIndex].content}</p>
-          </div>
+          <Card
+            style={{ marginTop: "10px" }}
+            title={`Slide ${currentSlideIndex + 1}`}
+            footer={footer}
+            header={header}
+            className="md:w-25rem"
+          >
+            <p className="m-0">
+              {presentation.slides[currentSlideIndex].content}
+            </p>
+          </Card>
         ) : (
-          <p>NO Slides</p>
+          <div>{noSlides}</div>
         )}
-        <button
-          onClick={handlePreviousSlide}
-          disabled={currentSlideIndex === 0}
-        >
-          Previous
-        </button>
-        <button
-          onClick={handleNextSlide}
-          disabled={currentSlideIndex === presentation.slides.length - 1}
-        >
-          Next
-        </button>
       </div>
-      <button onClick={handleBackToDashboard}>Back to Dashboard</button>
-      <form onSubmit={handleAddSlide}>
-        <h3>Add New Slide</h3>
-        <textarea
-          value={newSlideContent}
-          onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
-            setNewSlideContent(e.target.value)
-          }
-          placeholder="Slide content"
-        />
-        <button type="submit">Add Slide</button>
-      </form>
-      {presentation.slides.length > 0 && (
-        <div>
-          <h3>Edit Slide</h3>
-          <Dropdown
-            value={presentation.slides[currentSlideIndex]}
-            onChange={(e) =>
-              setCurrentSlideIndex(
-                presentation.slides.findIndex(
-                  (slide) => slide._id === e.target.value._id
-                )
-              )
-            }
-            options={presentation.slides}
-            optionLabel="slideNum"
-            placeholder="Select a City"
-            className="w-full md:w-14rem"
+      <Dialog
+        header="Add New Slide"
+        visible={isAddDialogVisible}
+        style={{ width: "50vw" }}
+        footer={
+          <>
+            <Button
+              label="Cancel"
+              onClick={() => closeDialog("add")}
+              className="p-button-text"
+            />
+            <Button label="Add" onClick={handleAddSlide} />
+          </>
+        }
+        onHide={() => closeDialog("add")}
+      >
+        <div className="p-field">
+          <label htmlFor="slides">Slide Content</label>
+          <InputTextarea
+            id="slides"
+            rows={5}
+            value={newSlideContent}
+            onChange={(e) => setNewSlideContent(e.target.value)}
           />
-
-          <select onChange={(e) => setSlideToEdit(Number(e.target.value))}>
-            <option value="">Select Slide to Edit</option>
-            {presentation.slides.map((slide, index) => (
-              <option key={slide._id} value={index}>
-                Slide {index + 1}
-              </option>
-            ))}
-          </select>
-          {slideToEdit !== null && (
-            <form onSubmit={handleAlterSlide}>
-              <textarea
-                value={editSlideContent}
-                onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
-                  setEditSlideContent(e.target.value)
-                }
-                placeholder="New slide content"
-              />
-              <button type="submit">Update Slide</button>
-              <button
-                type="button"
-                onClick={() => handleDeleteSlide(slideToEdit)}
-              >
-                Delete Slide
-              </button>
-            </form>
-          )}
         </div>
-      )}
+      </Dialog>
+      <Dialog
+        header="Edit Slide"
+        visible={isEditDialogVisible}
+        style={{ width: "50vw" }}
+        footer={
+          <>
+            <Button
+              label="Cancel"
+              onClick={() => closeDialog("edit")}
+              className="p-button-text"
+            />
+            <Button label="Edit" onClick={handleAlterSlide} />
+          </>
+        }
+        onHide={() => closeDialog("edit")}
+      >
+        <div className="p-field">
+          <label htmlFor="slides">Slide Content</label>
+          <InputTextarea
+            id="slides"
+            rows={5}
+            value={editSlideContent}
+            onChange={(e) => setEditSlideContent(e.target.value)}
+          />
+        </div>
+      </Dialog>
     </div>
   );
 }
