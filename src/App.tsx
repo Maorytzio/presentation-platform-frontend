46cb509bcd048c7f132c7a1a0d90eb51f574630a
maorbeeri <marmaor@gmail.com>
2024-08-21 14:01:24 +0300

    added DataTable to render list of presnetations in Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import axios from "axios";
 import Header from "./components/Header";
 import { useNavigate } from "react-router-dom";
 import PresentationForm from "./components/PresentationForm";
-import { Button } from 'primereact/button';
+
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 // Define interfaces for the data structure
 interface Slide {
@@ -18,7 +20,7 @@ interface Presentation {
 }
 
 function App() {
-  const [presentations, setPresentations] = useState<Presentation[]>([]);
+  const [presentations, setPresentations] = useState<Presentation[] | []>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -35,18 +37,48 @@ function App() {
     };
 
     (async () => await fetchPresentations())();
-  });
+  }, []);
 
   const handlePresentationClick = (title: string) => {
     navigate(`/presentation/${title}`);
   };
-          
 
   return (
     <div className="App">
       <Header />
-      <Button>sadf</Button>
-      {/* <button onClick={fetchPresentations}>Fetch Presentations</button> */}
+      <DataTable
+        value={presentations}
+        paginator
+        rows={5}
+        rowsPerPageOptions={[5, 10, 25, 50]}
+        tableStyle={{ minWidth: "50rem" }}
+        onRowClick={(e) => handlePresentationClick(e.data.title)}
+        selectionMode="single"
+      >
+        <Column field="title" header="Title" style={{ width: "25%" }}></Column>
+        <Column
+          field="authors"
+          header="Authors"
+          style={{ width: "25%" }}
+        ></Column>
+        <Column
+          field="slides.length"
+          header="Num of Slides"
+          style={{ width: "25%" }}
+        ></Column>
+        <Column
+          field="createdAt"
+          header="Publication Date"
+          style={{ width: "25%" }}
+          body={(rowData) =>
+            new Date(rowData.createdAt).toLocaleDateString("he-IL", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+            })
+          }
+        ></Column>
+      </DataTable>
       <ul>
         {presentations.map((presentation) => (
           <li
